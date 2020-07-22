360455e6262bf2c739832a80ea318d975ac0325a
RoboVij <roboVij83@gmail.com>
2020-07-22 10:45:03 +0200

    Version4

--- src/CoderStats.js
+++ src/CoderStats.js
@@ -1,19 +1,30 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { withStyles } from "@material-ui/styles";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/icons/Search";
 import styles from "./styles/CoderStatsStyles";
 
 class CoderStats extends Component {
   render() {
     const { classes } = this.props;
     return (
-      // <nav className={classes.nav}>
-      //     <h1 className={classes.heading}>React Colors</h1>
-      //     <Link to="/palette/new">Create Palette</Link>
-      //   </nav>
       <header className={classes.CoderStats}>
         <div className={classes.logo}>
           <h1>CoderStats</h1>
+        </div>
+        <div className={classes.search}>
+          <div className={classes.searchIcon}>
+            <SearchIcon />
+          </div>
+          <InputBase
+            placeholder="GitHub user name or organisation"
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+            inputProps={{ "aria-label": "search" }}
+          />
         </div>
       </header>
     );
--- src/styles/CoderStatsStyles.js
+++ src/styles/CoderStatsStyles.js
@@ -1,22 +1,59 @@
-export default {
+import { fade, makeStyles } from "@material-ui/core/styles";
+
+const styles = {
   CoderStats: {
-    display: "flex",
+    display: "block",
     alignItems: "center",
-    justifyContent: "flex-start",
+    justifyContent: "center",
+    fontSize: "22px",
     height: "6vh",
+    textAlign: "center",
   },
   logo: {
-    marginRight: "15px",
-    padding: "0 13px",
-    fontSize: "22px",
-    backgroundColor: "#eceff1",
-    fontFamily: "Roboto",
+    fontFamily: "Courier New",
+    // marginTop: "100px",
+    fontSize: "18px",
+    // fontFamily: "Roboto",
     height: "100%",
-    display: "flex",
+    // display: "flex",
     alignItems: "center",
     "& a": {
       textDecoration: "none",
       color: "black",
     },
   },
+  search: {
+    display: "flex",
+    textAlign: "center",
+    position: "absolute",
+    left: "50%",
+    top: "30%",
+    transform: "translate(-50%, -50%)",
+    borderRadius: "3%",
+    backgroundColor: "#eceff1",
+    margin: "20px",
+
+    "&:hover": {
+      backgroundColor: "#bcc1c4",
+    },
+  },
+  searchIcon: {
+    // padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    // pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    paddingLeft: "30px",
+    // padding: "50px",
+    fontSize: "30px",
+  },
 };
+
+export default styles;
